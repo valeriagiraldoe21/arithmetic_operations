45c4c6fc1a40d4a114105e3aea75e99be0bc13e3
valeria.giraldo <valeriagiraldoe21@javerianacali.edu.co>
2024-05-02 23:06:35 -0500

    a

---
+++ module.py
@@ -0,0 +1,7 @@
+def module(num_1, num_2):
+    if num_2 == 0:
+       print("Error: Module by zero is not allowed")
+       return None
+    result = num_1 % num_2
+    print(f'{num_1} % {num_2} is equal to {result}')
+    return result
